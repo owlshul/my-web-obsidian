1603a2c228a40c334dd4ccecdbc4df432c882789
Anshul Gurjar <anshul7443@gmail.com>
2026-05-17 05:16:52 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
-const MongoStore = require('connect-mongo').default || require('connect-mongo');
+const connectMongo = require('connect-mongo');
+const MongoStore = typeof connectMongo.create === 'function' ? connectMongo : connectMongo.default;
 const mongoose = require('mongoose');
 const path = require('path');
 const { marked } = require('marked');
